cffb0db4934b648c023f3fd7c71a1647535947d5
Mark Roland <radiowhir@gmail.com>
2018-08-18 21:44:44 -0700

    Refactored for cleanup and graceful failure of API

---
+++ README.md
@@ -0,0 +1,9 @@
+# Weather Widget
+
+This project translates weather data into a graphical visualization.
+
+The artwork dates back to roughly 2005.
+
+The Yahoo Weather API was added in 2011.
+
+The code was refactored in 2018 to fail gracefully since the Yahoo Weather API has been disabled.
--- index.php
+++ index.php
@@ -1,140 +1,120 @@
 <?php
 /*
-  Weather Widget
+Weather Widget
 
-  This script interprets a Yahoo Weather API data feed and renders the current
-  weather conditions as an informational graphic
-
-  Mark Roland <mark at markroland dot com>
-
-  9/24/2011 - Completed
+This script interprets a Yahoo Weather API data feed and renders the current
+weather conditions as an informational graphic
 
 */
+
+// ---> 0) Initialize Data
+
+$weather = [
+    'text' => 'Unknown',
+    'temp' => 'Unknown',
+    'humidity' => 'Unknown',
+    'image' => 'images/unknown.png'
+];
 
 // ---> 1) Get the data
 
 // Create a new cURL resource
 $ch = curl_init();
-
 curl_setopt($ch, CURLOPT_URL, 'http://weather.yahooapis.com/forecastrss?w=12786731');
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-// Grab URL and pass it to the browser
 $data = curl_exec($ch);
-
-// Close cURL resource, and free up system resources
 curl_close($ch);
 
-// View response XML
-//echo "<pre>"; var_dump($data); echo "</pre>";
+// Only process response if it is not empty
+if ($data) {
 
-// Read in XML response to test for the last page
-$weather = simplexml_load_string($data);
+    // Read in XML response to test for the last page
+    $weather_xml = simplexml_load_string($data);
 
+    // ---> 2) Parse the data
 
+    /*
+        The following section of code was taken from these web sites on 9/18/2011. They
+        show how to capture custom namespace variables from XML.
+        http://pkarl.com/articles/parse-yahoo-weather-rss-using-php-and-simplexml-al/
+        http://snipt.net/pkarl/parse-yahoo-weather-feeds-with-simplexml-php
+    */
 
-// ---> 2) Parse the data
+    $channel_yweather = $weather_xml->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0");
+    foreach($channel_yweather as $x => $channel_item){
+        foreach($channel_item->attributes() as $k => $attr) {
+            $yw_channel[$x][$k] = $attr;
+        }
+    }
 
-/*
-  The following section of code was taken from these web sites on 9/18/2011. They
-  show how to capture custom namespace variables from XML.
-  http://pkarl.com/articles/parse-yahoo-weather-rss-using-php-and-simplexml-al/
-  http://snipt.net/pkarl/parse-yahoo-weather-feeds-with-simplexml-php
-*/
+    $yw_forecast = array();
+    $item_yweather = $weather_xml->channel->item->children("http://xml.weather.yahoo.com/ns/rss/1.0");
+    foreach($item_yweather as $x => $yw_item) {
+        foreach($yw_item->attributes() as $k => $attr) {
+            if($k == 'day')
+                $day = $attr;
+            if($x == 'forecast'){
+                $yw_forecast[$x][$day . ''][$k] = $attr;
+            }else{
+                $yw_forecast[$x][$k] = $attr;
+            }
+        }
+    }
 
-$channel_yweather = $weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0");
-foreach($channel_yweather as $x => $channel_item){
-	foreach($channel_item->attributes() as $k => $attr)
-		$yw_channel[$x][$k] = $attr;
+    if (isset($yw_forecast)) {
+        $weather['text'] = $yw_forecast['condition']['text'];
+    }
+    if (isset($yw_forecast)) {
+        $weather['text'] = $yw_forecast['condition']['text'];
+    }
+
+    $weather['humidity'] = (string)$weather_xml->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0")->atmosphere->attributes()->humidity;
+
+    // ---> 3) Translate the data
+
+    // Parse weather codes, associating code to image
+    if (($handle = fopen(realpath(__DIR__ . '/data' . '/yahoo-weather-codes.csv'), "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $weather_image[$data[0]] = $data[2];
+        }
+        fclose($handle);
+    }
+    $weather_image_index = trim($yw_forecast['condition']['code']);
+    $weather['image'] = 'images/' . $weather_image[$weather_image_index];
 }
-
-
-$item_yweather = $weather->channel->item->children("http://xml.weather.yahoo.com/ns/rss/1.0");
-foreach($item_yweather as $x => $yw_item) {
-	foreach($yw_item->attributes() as $k => $attr) {
-		if($k == 'day')
-		  $day = $attr;
-		if($x == 'forecast'){
-		  $yw_forecast[$x][$day . ''][$k] = $attr;
-		}else{
-		  $yw_forecast[$x][$k] = $attr;
-		}
-	}
-}
-
-
-// ---> 3) Translate the data
-
-// Parse weather codes, associating code to image
-$row = 1;
-if(($handle = fopen("yahoo-weather-codes.csv", "r")) !== FALSE){
-  while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-    $weather_image[$data[0]] = $data[2];
-  }
-  fclose($handle);
-}
-
-// Save the weather code as a usable index for the $weather_image array
-$weather_image_index = trim($yw_forecast['condition']['code']);
-
-
-// ---> 4) Render the data
-
-
-
-// Create image HTML
-print('<!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset=utf-8>
-	<title>Weather Widget</title>
-	<style type="text/css">
-	  #weather_widget{
-      width: 560px;
-      margin: 10px auto;
-      padding: 0 10px;
-      border: 1px solid #CCC;
-      background-color: #FFF;
-      font-family: Helvetica, Arial, Sans-serif;
-      text-align: center;
-      -webkit-border-radius: 10px;
-      -moz-border-radius: 10px;
-      border-radius: 10px;
-      box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
-      -webkit-box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
-      -moz-box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
-	  }
-	  #weather_widget h1{
-	    font-size: 18px;
-	  }
-	  #weather_widget p{
-	    font-size: 14px;
-	  }
-	</style>
-</head>
-
-<body>'."\n");
-print("\t".'<div id="weather_widget">'."\n");
-print("\t\t<h1>Current Weather Conditions in Lawrence, Kansas</h1>\n");
-printf("\t\t".'<img src="images/%s" width="%d" height="%d" alt="%s" />'."\n",
-        $weather_image[$weather_image_index],
-        512, 200,
-        'Weather Conditions: '.$yw_forecast['condition']['text'] );
-
-printf("\t\t".'<p><b>%s</b> &#8212; %s&deg;F</p>'."\n",
-  $yw_forecast['condition']['text'],
-  $yw_forecast['condition']['temp'],
-  (string)$weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0")->atmosphere->attributes()->humidity
-);
-  
-
-//printf("\t".'<p>Temperature: %s&deg; F</p>'."\n", $yw_forecast['condition']['temp']);
-//printf("\t".'<p>Humidity: %s%%</p>'."\n", (string)$weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0")->atmosphere->attributes()->humidity);
-//printf("\t".'<p>Sunrise: %s</p>'."\n", (string)$weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0")->astronomy->attributes()->sunrise);
-//printf("\t".'<p>Sunrise: %s</p>'."\n", (string)$weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0")->astronomy->attributes()->sunset);
-print("\t".'</div>
-</body>
-</html>');
 
 ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset=utf-8>
+    <title>Weather Widget</title>
+    <style type="text/css">
+        #weather_widget{
+            width: 560px;
+            margin: 10px auto;
+            padding: 0 10px;
+            border: 1px solid #CCC;
+            background-color: #FFF;
+            font-family: Helvetica, Arial, Sans-serif;
+            text-align: center;
+            border-radius: 10px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+        }
+        #weather_widget h1{
+            font-size: 18px;
+        }
+        #weather_widget p{
+            font-size: 14px;
+        }
+    </style>
+</head>
+<body>
+    <div id="weather_widget">
+        <h1>Current Weather Conditions in Lawrence, Kansas</h1>
+        <?php printf('<img src="%s" width="512" height="200" alt="%s" />', $weather['image'], 'Weather Conditions: ' . $weather['text']); ?>
+        <?php printf('<p><b>%s</b> &#8212; %s&deg;F</p>', $weather['text'], $weather['temp'], $weather['humidity']); ?>
+    </div>
+</body>
+</html>
